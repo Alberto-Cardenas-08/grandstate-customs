60303d84dd02f225704ce839cc3cf6051085f3e0
Alberto Cardenas <al05059229@tecmilenio.mx>
2026-02-27 08:54:13 -0500

    actualizacion db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,15 @@
-// bd.js
 const mongoose = require('mongoose');
-const dns = require('dns');
-
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+const dns = require('dns'); 
+dns.setServers(['8.8.8.8', '1.1.1.1']);  
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB Atlas conectado correctamente');
-  } catch (err) {
-    console.error('Error al conectar con MongoDB Atlas:', err);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);  
+        console.log('MongoDB conectado');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
